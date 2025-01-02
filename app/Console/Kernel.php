faba0ff18858600a879c954fd1e05173ed871fa5
mrttuncer <mert_tuncer123@hotmail.com>
2025-01-03 02:54:37 +0300

    add kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        // Planlanan komutlarınızı buraya ekleyin
+
         $schedule->command('app:check-appointment-date')->everyFiveMinutes();
     }
 
