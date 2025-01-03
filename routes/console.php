9e4795cb8f1e2f555496b9a6f114cbc181b6eee7
mrttuncer <mert_tuncer123@hotmail.com>
2025-01-03 03:02:25 +0300

    update console

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('app:check-appointment-date', function () {
+
+    $this->info('Checking appointment dates...');
+})->purpose('Check appointment dates and process them.')
+    ->everyFiveMinutes();
